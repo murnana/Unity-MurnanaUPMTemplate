c50f0b2127fd7bc57282b084d77f687522c6585b
murnana <murnana@users.noreply.github.com>
2023-03-18 10:21:47 +0900

    edi Project for Unity 2020.3

--- Packages/com.murnana.custom-upm/Editor/Properties/AssemblyInfo.cs
+++ Packages/com.murnana.custom-upm/Editor/Properties/AssemblyInfo.cs
@@ -1,1 +1,8 @@
+// Assembly Title
+[assembly: System.Reflection.AssemblyTitleAttribute("Murnana Custom Editor")]
+
+// Copyright
+[assembly: System.Reflection.AssemblyCopyrightAttribute("© 2023 Murnana")]
+
+// Assembly is UnityEditor assembly
 [assembly: UnityEngine.AssemblyIsEditorAssembly]
---
+++ Packages/com.murnana.custom-upm/Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// Assembly Title
+[assembly: System.Reflection.AssemblyTitleAttribute("Murnana Custom")]
+
+// Copyright
+[assembly: System.Reflection.AssemblyCopyrightAttribute("© 2023 Murnana")]
+
+// Internal visible to self Editor's assembly
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("Murnana.Custom.Editor")]
